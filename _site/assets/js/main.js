5b65500f2354bc2e48db55183ebb8e191c8b0fb7
Olivia <cozymonster29@gmail.com>
2018-06-27 22:17:58 +0200

    will ignore if there is no medium or style

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,5 +1,30 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+function showMedium() {
+    $("#project-medium").css({display: 'block'});
+    $("#project-medium-hr").css({display: 'block'});
+}
+
+function showStyle() {
+    $("#project-style").css({display: 'block'});
+    $("#project-style-hr").css({display: 'block'});
+}
+
+function hideMedium() {
+    $("#project-medium").css({display: 'none'});
+    $("#project-medium-hr").css({display: 'none'});
+}
+
+function hideStyle() {
+    $("#project-style").css({display: 'none'});
+    $("#project-style-hr").css({display: 'none'});
+}
+
+$(function() {
+    hideMedium()
+    hideStyle()
+});
+
 new AnimOnScroll( document.getElementById( 'grid' ), {
     minDuration : 0.4,
     maxDuration : 0.7,
@@ -18,30 +43,54 @@ $(".modal-link").animatedModal({
 });
 
 $('.modal-link').on('click', function(event) {
+    var target = event.currentTarget
     $('#project-img').hide()
-    $('#project-img').attr('src', event.currentTarget.getAttribute("data-image-url"));
-    $("#project-title").text(event.currentTarget.getAttribute("data-title"));
-    $("#project-desc").text(event.currentTarget.getAttribute("data-desc"));
-    $("#project-medium").text(event.currentTarget.getAttribute("data-medium"));
-    $("#project-style").text(event.currentTarget.getAttribute("data-style"));
+    $('#project-img').attr('src', target.getAttribute("data-image-url"));
+    $("#project-title").text(target.getAttribute("data-title"));
+    $("#project-desc").text(target.getAttribute("data-desc"));
+    
+    var medium = target.getAttribute("data-medium");
+    
+    if (medium) {
+        console.log(medium)
+        showMedium()
+        $("#project-medium").text(medium);
+    }
 
-    var date = new Date(event.currentTarget.getAttribute("data-date"))
+    var style = target.getAttribute("data-style")
+
+    if (style) {
+        console.log(style)
+        showStyle()
+        $("#project-style").text(style);
+    }
+
+    var medium = target.getAttribute("data-style")
+    $("#project-style").text();
+
+    var date = new Date(target.getAttribute("data-date"))
     
     $("#project-date").text(months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear());
 
-    $("#loading").show();
+    setTimeout(
+    function() {
+        $("#loading").show();
+    }, 800); // 800 is same number ms as animatedModal fadeOut
 
     $('#project-img').on('load', function(){
         $("#loading").hide();
-      $('#project-img').fadeIn("slow")
+      $('#project-img').fadeIn()
     });
 
 });
 
 $('.close-animatedModal').on('click', function(event) {
+    $("#loading").hide();
     setTimeout(
     function() {
+        hideMedium()
+        hideStyle()
         $('#project-img').attr('src', '');
         document.getElementById('animatedModal').scrollTop = 0;
-    }, 300); // 600 is same number ms as animatedModal fadeOut
+    }, 300);
 });
