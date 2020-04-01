6a9b7f0f267c1532ee8be6bb4f2100309256684f
Olivia <oliviachang29@gmail.com>
2020-03-31 17:15:28 -0700

    fix images not unveiling

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,13 @@ function initJS() {
 
   scroll = new SmoothScroll();
   
-  $(".lazy").unveil(0, function() {
+  $(".lazy").unveil(100, function() {
     $(this).on('load', function() {
       this.style.opacity = 1;
     });
   });
+
+  $("img").trigger("unveil");
 
   markCurrentLink()
 }
